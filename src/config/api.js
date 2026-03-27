b2ed3622e48fb1a4d2f235fb315ab94dc3f7112d
gangaraju2005 <karaganagangaraju2005@gmail.com>
2026-03-27 10:20:11 +0530

    final commit

--- src/config/api.js
+++ src/config/api.js
@@ -4,5 +4,6 @@
 // const BASE_URL = "http://192.168.1.117:8000/api";
 // const BASE_URL = "http://98.92.128.133:8000";
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL;
+// const API_URL = process.env.EXPO_PUBLIC_API_URL;
+const API_URL = "http://98.92.128.133/api";
 export default API_URL;
